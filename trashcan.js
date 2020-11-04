002c0406532a969eeca8ed44f7b4d6e44afcfa66
Aditya783123 <72315477+Aditya783123@users.noreply.github.com>
2020-11-04 14:38:53 -0800

    Add files via upload

---
+++ trashcan.js
@@ -0,0 +1,21 @@
+
+class Trashcan {
+    constructor(x,y,width, height){
+    var trashCanOptions = {
+        isStatic:true
+    };
+    this.body = Bodies.rectangle(x,y,width,height, trashCanOptions); 
+    this.height = height;
+    this.width = width;
+    this.image = loadImage("Sprites/dustbingreen.png");
+    World.add(world, this.body);
+    }
+    
+    display(){
+    var pos = this.body.position;
+    rectMode(CENTER);
+    fill("red");
+    rect(pos.x, pos.y, this.width, this.height);
+    }
+    
+}
